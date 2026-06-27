f0c677398c60154263d3fba8b0fa458dc9303435
Richard12434 <uni@oppenhorst.com>
2026-06-27 21:39:58 +0200

    Delete db directory

--- db/03_indexes.sql
+++
@@ -1,4 +0,0 @@
-CREATE INDEX idx_gebaeude_campus ON gebaeude(campus_id);
-CREATE INDEX idx_etage_gebaeude ON etage(gebaeude_id);
-CREATE INDEX idx_raum_etage ON raum(etage_id);
-CREATE INDEX idx_eingang_gebaeude ON eingang(gebaeude_id);
--- db/04_seed.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO campus (campus_name, umriss)
-VALUES ('Halle Campus', 'POLYGON(...)');
-
-INSERT INTO gebaeude (gebaeude_name, umriss, campus_id)
-VALUES ('Hauptgebäude', 'POLYGON(...)', 1);
