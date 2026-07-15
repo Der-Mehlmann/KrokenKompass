b1e79c506c32d4e100d97f6572818318e5365aae
Niklas Mehlis <75626426+Der-Mehlmann@users.noreply.github.com>
2026-07-16 01:43:06 +0200

    Inject version tag into index.html and update footer to display version
    Signed-off-by: Niklas Mehlis <75626426+Der-Mehlmann@users.noreply.github.com>

--- Code/frontend/src/Main.elm
+++ Code/frontend/src/Main.elm
@@ -94,10 +94,11 @@ type alias Model =
     , shake : Bool
     , aktuelleEtage : String
     , currentFloor : String
+    , version : String
     }
 
-init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
-init _ url key =
+init : String -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
+init versionFlag url key =
     ( { key = key
       , url = url
       , route = parseUrl url
@@ -110,6 +111,7 @@ init _ url key =
       , shake = False
       , aktuelleEtage = "00"
       , currentFloor = "EG / 0"
+      , version = versionFlag
       }
     , Cmd.batch [ fetchGraph, fetchVspUnits ]
     )
@@ -586,7 +588,7 @@ viewHome model =
                 ]
             ]
         , viewAbstractCards
-        , viewFooter
+        , viewFooter model
         ]
 
 viewAbstractCards : Html Msg
@@ -655,7 +657,7 @@ viewPlanner model =
                 , viewRoutePlanner model
                 ]
             ]
-        , viewFooter
+        , viewFooter model
         ]
 
 viewHeader : Model -> Html Msg
@@ -738,8 +740,8 @@ viewHeader model =
             ]
         ]
 
-viewFooter : Html Msg
-viewFooter =
+viewFooter : Model -> Html Msg
+viewFooter model =
     footer [ class "footer-custom py-5 px-6 is-flex is-justify-content-space-between is-align-items-center", style "border-top" "1px solid var(--bulma-border-weak)" ]
         [ div [ class "is-flex is-align-items-center footer-links-custom" ]
             [ Svg.svg
@@ -751,6 +753,18 @@ viewFooter =
                 ]
                 [ Svg.path [ SvgAttr.d "M12 0C12 6.62742 17.3726 12 24 12C17.3726 12 12 17.3726 12 24C12 17.3726 6.62742 12 0 12C6.62742 12 12 6.62742 12 0Z" ] [] ]
             , span [ class "has-text-weight-medium is-size-5" ] [ text "KrokenKompass" ]
+            ]
+        , div [ class "is-flex is-align-items-center" ]
+            [ a [ href "https://github.com/Der-Mehlmann/KrokenKompass", target "_blank", class "has-text-grey mr-3 is-flex is-align-items-center" ]
+                [ Svg.svg
+                    [ SvgAttr.viewBox "0 0 24 24"
+                    , SvgAttr.width "20"
+                    , SvgAttr.height "20"
+                    , SvgAttr.fill "currentColor"
+                    ]
+                    [ Svg.path [ SvgAttr.d "M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" ] [] ]
+                ]
+            , span [ class "has-text-grey is-size-7" ] [ text ("v" ++ model.version) ]
             ]
         ]
 
